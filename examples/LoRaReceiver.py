c9501de45cdffbb1c391c9a7fe08cbb4174b6e30
JustDr00py <115050378+JustDr00py@users.noreply.github.com>
2022-11-15 20:35:26 -0800

    Update LoRaReceiver.py

--- examples/LoRaReceiver.py
+++ examples/LoRaReceiver.py
@@ -2,17 +2,13 @@ from machine import Pin, SoftI2C
 from ssd1306 import SSD1306_I2C
 from time import sleep
 
-
 oledSDA = Pin(15, Pin.OUT, Pin.PULL_UP)
 oledSCL = Pin(4, Pin.OUT, Pin.PULL_UP)
 oledRST = Pin(16, Pin.OUT)
 oledRST.value(1)
+i2c = SoftI2C(oledSDA, oledSCL)
+oled = SSD1306_I2C(128, 64, i2c)
 
-i2c = SoftI2C(scl=oledSDA, sda=oledSCL)
-
-oled_width = 128
-oled_height = 64
-oled = SSD1306_I2C(oled_width, oled_height, i2c)
 def receive(lora):
     print("LoRa Receiver")
     oled.fill(0)
